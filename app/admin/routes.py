b3cde8a0dcc445560dae56456eda87345b8f453a
Kaung Set Lin <kaung@macbookpro.local>
2022-07-24 21:46:52 -0700

    admin menu crud

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,5 +1,5 @@
 from app.admin import bp
-from app.admin.utils import allowed_file
+from app.admin.utils import allowed_file, to_int
 from flask import render_template, request, flash, current_app, redirect, url_for
 from db import query_db, get_db, to_object
 from werkzeug.utils import secure_filename
@@ -35,6 +35,43 @@ def updatePaid(id, paid):
 
 @bp.route('/menu', methods=['GET', 'POST'])
 def menu():
+    menu = query_db('select id, name, category, price, description, is_special, is_active from menu')
+    return render_template('admin/menu.html', title='Menu', menu=menu)
+    
+@bp.route('/menu/update/<int:id>', methods=['GET', 'POST'])
+def update_item(id):
+    item = query_db('select * from menu where id = ?', (id,), one=True)
+    if not item: abort(404)
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        category = request.form.get('category')
+        price = request.form.get('price')
+        description = request.form.get('description')
+        description = description.strip() if description else description
+        image = request.files.get('image')
+        special = request.form.get('special')
+        special = to_int(special)
+        active = request.form.get('active')
+        active = to_int(active)
+        filename = None
+
+        if image and allowed_file(image.filename):
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+        else:
+            filename = item['image']
+
+
+        query_db('''update menu set name=?, category=?, price=?, description=?, image=?, is_special=?, is_active=? where id=?''',
+                (name, category, price, description, filename, special, active, id))
+        get_db().commit()
+        flash('Menu item has been updated')
+        return redirect(url_for('admin.update_item', id=id))
+    return render_template('admin/update_item.html', title=f'Item #{item["id"]:02d}', item=item) 
+
+@bp.route('/menu/add', methods=['GET', 'POST'])
+def add_item():
     if request.method == 'POST':
         name = request.form.get('name')
         category = request.form.get('category')
@@ -46,9 +83,18 @@ def menu():
             filename = secure_filename(image.filename)
             image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
 
-        query_db('''insert into menu (name, category, price, description, image)
+        query_db('''insert or replace into menu (name, category, price, description, image)
                 values (?, ?, ?, ?, ?)''',
                 (name, category, price, description, secure_filename(image.filename)))
         get_db().commit()
         flash('Menu item has been added')
-    return render_template('admin/menu.html', title='Menu')
+    return render_template('admin/add_item.html', title='Add Menu Item')
+
+@bp.route('/menu/delete/<int:id>', methods=['POST'])
+def delete_item(id):
+    item = query_db('select * from menu where id = ?', (id,), one=True)
+    if not item: abort(404)
+    query_db('delete from menu where id = ?', (id,))
+    get_db().commit()
+    flash(f"Item #{id:02d} has been deleted")
+    return redirect(url_for('admin.menu'))
--- app/admin/utils.py
+++ app/admin/utils.py
@@ -1,3 +1,11 @@
 def allowed_file(filename):
     extensions = {'png', 'jpg', 'jpeg'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in extensions
+
+def to_int(checkbox):
+    if checkbox == 'on':
+        return str(1)
+    elif not checkbox:
+        return str(0)
+    else: return checkbox
+
